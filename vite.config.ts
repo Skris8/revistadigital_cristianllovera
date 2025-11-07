693e10e1b95e8de2c5f87bd793b2b0299340e709
Cristian Llovera <lloveracristian@gmail.com>
2025-11-07 01:34:49 -0400

    todo funciona localmente

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,12 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [vue()],
+  // Base correcta para GitHub Pages (solo en build)
+  base: command === "serve" ? "/" : "/revistadigital_cristianllovera/",
   build: {
-    copyPublicDir: false,
-    lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
-      name: "FlipbookVue3",
-      // the proper extensions will be added
-      fileName: "flipbook-vue3",
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["vue"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: "Vue",
-        },
-      },
-    },
+    outDir: "dist",
+    copyPublicDir: true, // si tienes imágenes en public/
   },
-});
+}));
