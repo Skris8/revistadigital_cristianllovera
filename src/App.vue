abf1bbbafea688a3b41d84407d4988082a5966b9
Cristian Llovera <lloveracristian@gmail.com>
2025-11-07 00:16:55 -0400

    Initial commit - revista flipbook

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,16 @@ import IconArrow from "./components/IconArrow.vue";
 
 // Array of image paths
 const pages = ref([
-  "/images/1.jpg",
-  "/images/2.jpg",
-  "/images/3.jpg",
-  "/images/4.jpg",
-  "/images/5.jpg",
-  "/images/6.jpg",
-  "/images/7.jpg",
-  "/images/8.jpg",
-  "/images/9.jpg",
+  "/images/1.png",
+  "/images/2.png",
+  "/images/3.png",
+  "/images/4.png",
+  "/images/5.png",
+  "/images/6.png",
+  "/images/7.png",
+  "/images/8.png",
+  "/images/9.png",
+  "/images/10.png",
 ]);
 </script>
 
